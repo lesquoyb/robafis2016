9981e843740ce86e7d98763f83f373e526e3124f
lesquoyb <nouvelleaddresse@yahoo.fr>
2016-08-17 20:19:41 +0200

    yolo

--- src/robot/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * @author Jimmy
- *
- */
-package robot;
--- src/robot/sensors/Sensor.java
+++
@@ -1,8 +0,0 @@
-package robot.sensors;
-
-public interface Sensor {
-	void setPort(String port);
-	float getValue();
-	void reset();
-	void close();
-}
